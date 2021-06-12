21691612606810491c3b48d0e83b1cd2ada2c539
Shadowmaple <1142319190@qq.com>
2021-06-12 16:21:38 +0800

    new

---
+++ chapter11/p11-2.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+# include<arpa/inet.h>
+
+int main() {
+	// char *src = "127.0.0.1";
+	// void *dest = "1";
+	// int res = inet_pton(AF_INET, src, dest);
+	// printf("%d\n", res);
+	return 0;
+}
